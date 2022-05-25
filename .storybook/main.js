4f0712119c1d7413f9fbb51717182ef94e5a7134
Andres Valdez <andresevaldez@gmail.com>
2022-05-25 14:03:22 -0500

    configuration complete with web browser and app

---
+++ .storybook/main.js
@@ -0,0 +1,21 @@
+// NO NEED TO MESS WITH THIS FOR REACT-NATIVE
+
+// module.exports = {
+//   addons: [
+//     '@storybook/addon-essentials',
+//     'storybook/addon-actions',
+//     'storybook/addon-links',
+//     'storybook/addon-knobs',
+//   ],
+//   babel: async (options) => ({
+//     // Update your babel configuration here
+//     ...options,
+//   }),
+//   framework: '@storybook/react',
+//   stories: ['../storybook/**/*.stories.@(js|mdx)'],
+//   webpackFinal: async (config, { configType }) => {
+//     // Make whatever fine-grained changes you need
+//     // Return the altered config
+//     return config;
+//   },
+// };
