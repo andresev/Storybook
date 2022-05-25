a5d3175a22d65a2a58a2d617869d12e5e29964fa
Andres Valdez <andresevaldez@gmail.com>
2022-05-25 12:25:25 -0500

    initial react-native setup

---
+++ storybook/rn-addons.js
@@ -0,0 +1,4 @@
+// These were changed to '/register' instead of '/manager'
+import '@storybook/addon-ondevice-actions/register';
+import '@storybook/addon-ondevice-knobs/register';
+//import '@storybook/addon-ondevice-actions/register';
