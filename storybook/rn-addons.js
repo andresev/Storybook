24e86cc5f13106591ff7d5a0ee22a5ba44740e95
Andres Valdez <andresevaldez@gmail.com>
2022-05-25 12:46:31 -0500

    setup complete - ready for additional components

--- storybook/rn-addons.js
+++ storybook/rn-addons.js
@@ -1,4 +1,5 @@
 // These were changed to '/register' instead of '/manager'
 import '@storybook/addon-ondevice-actions/register';
 import '@storybook/addon-ondevice-knobs/register';
+
 //import '@storybook/addon-ondevice-actions/register';
