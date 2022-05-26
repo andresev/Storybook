499856a418e6f87c304d4115555143d763583010
Andres Valdez <andresevaldez@gmail.com>
2022-05-26 15:38:33 -0500

    fixed import bugs

--- src/components/Component.stories.js
+++ src/components/Component.stories.js
@@ -2,19 +2,15 @@ import React from 'react';
 import { storiesOf } from '@storybook/react-native';
 import CenterView from '../../storybook/stories/CenterView';
 import { action } from '@storybook/addon-actions';
-import { button, knob, object, text } from '@storybook/addon-knobs';
+import { button, object, text } from '@storybook/addon-knobs';
 
 import TestComponent from './buttons/TestComponent';
 import Button from './buttons/Button';
 import MainSlider from './Slider/MainSlider';
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
-import CalendarPicker from 'react-native-calendar-picker';
 
 storiesOf('Test Component', module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
-  .add('example', () => (
-    <CalendarPicker onDateChange={action('onDateChange')} />
-  ));
+  .add('example', () => <TestComponent />);
 
 storiesOf('Buttons', module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
